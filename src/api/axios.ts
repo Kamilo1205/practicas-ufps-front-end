6f53cd6118cd825371c3293eecaa291a672b139d
Kamilo1205 <kamilo_1205@outlook.com>
2024-04-30 18:49:16 -0500

    crear instancia de axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,45 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_URL,
   withCredentials: true, // Importante para el envío de cookies en cada solicitud
 });
 
-export default instance;
+axiosInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    if (error.response && error.response.status === 401 && !error.config.__isRetryRequest) {
+      // El token ha expirado o es inválido, intenta renovarlo
+      let retries = 0;
+      const maxRetries = 3;
+      while (retries < maxRetries) {
+        try {
+          // Agrega encabezados para evitar el almacenamiento en caché de la respuesta
+          const config = {
+            ...error.config,
+            headers: {
+              ...error.config.headers,
+              'Cache-Control': 'no-cache',
+              Pragma: 'no-cache',
+              Expires: '0',
+            },
+          };
+          await axios.get('/auth/refresh', config);
+          error.config.__isRetryRequest = true;
+          return axios(error.config);
+        } catch (refreshError) {
+          retries++;
+          console.error(`Error al renovar el token, intento ${retries} de ${maxRetries}:`, refreshError);
+        }
+      }
+      console.error('No se pudo renovar el token después de varios intentos');
+      // Manejo adicional o redirigir al usuario al inicio de sesión
+      return Promise.reject(error);
+    }
+    // Si no es un error de autorización o no se puede renovar el token, rechaza la solicitud con el error original
+    return Promise.reject(error);
+  }
+);
+
+
+export default axiosInstance;
