3f19d3b4691a61bfffa73a9dfc72b411e8544e88
Kamilo1205 <kamilo_1205@outlook.com>
2024-05-05 20:47:32 -0500

    agregar estilos

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,38 +1,33 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from 'react';
+import { cva } from 'class-variance-authority';
+import { cn } from '../../utils';
 
-type ButtonVariant = 'primary' | 'secondary' | 'danger';
-
+const buttonStyles = cva(
+  ["flex", "w-full", "text-sm", "rounded-md", "leading-6", "px-3", "shadow-sm", "py-1.5 ", "items-center", "justify-center", "font-semibold", "focus-visible:outline", "focus-visible:outline-2", "focus-visible:outline-offset-2"],
+  {
+    variants: {
+      variant: {
+        default: ["bg-indigo-600", "text-white", "hover:bg-indigo-500", "focus-visible:outline-indigo-600"],
+        outline: ["border border-inherit"],
+      },
+    },
+    defaultVariants: {
+      variant: "default"
+    },
+  }
+);
+  
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariant;  // Opcional, establece un estilo predeterminado
+  variant?: "default" | "outline";
 }
 
-
-export const Button = ({children, variant = 'primary', className, ...props}: ButtonProps) => {
-    const baseStyle = "px-4 py-2 font-bold text-white rounded focus:outline-none focus:shadow-outline";
-    let variantStyle = "";
-  
-    switch (variant) {
-      case 'primary':
-        variantStyle = "bg-blue-500 hover:bg-blue-700";
-        break;
-      case 'secondary':
-        variantStyle = "bg-gray-500 hover:bg-gray-700";
-        break;
-      case 'danger':
-        variantStyle = "bg-red-500 hover:bg-red-700";
-        break;
-      default:
-        variantStyle = "bg-blue-500 hover:bg-blue-700";
-    }
-  
-    const combinedClassName = `${baseStyle} ${variantStyle} ${className || ''}`;
-  
+export const Button = ({children, variant, className, ...props}: ButtonProps) => {
+    // flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+    // mt-5 flex gap-x-2 w-full justify-center items-center border border-inherit rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+    
     return (
-      <button
-        className={combinedClassName}
-        {...props}
-      >
-        {children}
+      <button className={cn(buttonStyles({ variant, className}))} {...props}>
+        { children }
       </button>
     );
 }
