89ae107cf55dd64daa34ca07e335b9d21c3890f8
Jeison Ferrer <jeison120120@hotmail.com>
2024-06-03 19:26:09 -0500

    calendario en forma de tabla

--- src/routes/CoordinadorRouter.tsx
+++ src/routes/CoordinadorRouter.tsx
@@ -3,6 +3,7 @@ import { Layout } from '../layouts/Layout';
 import { CoordinadorScreen } from '../pages/coordinador/CoordinadorScreen';
 import { UsuariosPage } from '../pages/coordinador/UsuariosPage';
 import { EmpresaPage } from '../pages/coordinador/EmpresasPages';
+import { CalendarioPage } from '../pages/coordinador/CalendarioPage';
 
 export const CoordinadorRouter = () => {
   return (
@@ -11,6 +12,7 @@ export const CoordinadorRouter = () => {
           <Route index path='/' element={<CoordinadorScreen />} />
           <Route index path='/usuarios' element={<UsuariosPage />} />
           <Route index path='/empresas' element={<EmpresaPage />} />
+          <Route index path='/calendario' element={<CalendarioPage />} />
           <Route path="*" element={<Navigate to="/coordinador" replace />} />
         </Route>
     </Routes>
