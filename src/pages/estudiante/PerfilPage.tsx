0cd403cb36f5deaf515aa203b63594681d1ca886
Jeison Ferrer <jeison120120@hotmail.com>
2024-09-06 08:54:54 -0500

    feedback loading para perfil estudiante - Ahora el perfilPage tiene un spinner para indicar que se está carganado

--- src/pages/estudiante/PerfilPage.tsx
+++ src/pages/estudiante/PerfilPage.tsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import useEstudiantes from '../../hooks/useEstudiantes';
 import { EstudiantePerfilComponent } from '../../components/usuarios/perfil/EstudiantePerfilComponent';
 import { useAuth } from '../../contexts';
+import { set } from 'zod';
+import LoadingSpinner from '../../components/ui/Pagination/LoadingSpiner';
 
 export const PerfilPage = () => {
 
+  const [loading, setLoading] = useState(true)
   const { estudiante, fetchEstudiante } = useEstudiantes();
   const cargarEstudiante = async () => {
     console.log('se dispara')
     await fetchEstudiante();
+    setLoading(false)
   }
   useEffect(() => {
     if (!estudiante) cargarEstudiante();
@@ -17,6 +21,9 @@ export const PerfilPage = () => {
 
   return (
     <div>
+      {loading && <div className='w-full h-dvh flex justify-center align-middle'>
+        <LoadingSpinner />
+      </div>}
       {estudiante && <EstudiantePerfilComponent estudiante={estudiante} />}
 
     </div>
