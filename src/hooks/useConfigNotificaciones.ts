e776dcecda54350a52d4d41a9765488f4c67b7ed
Jeison Ferrer <jeison120120@hotmail.com>
2024-09-09 18:15:58 -0500

    ya las notificaciones y los primeros pasos están correctos

--- src/hooks/useConfigNotificaciones.ts
+++ src/hooks/useConfigNotificaciones.ts
@@ -9,6 +9,8 @@ import useEstudiantes from "./useEstudiantes";
 import { Estudiante } from "../interfaces/estudiante.interface";
 import { useGeneralConfig } from "./useGeneralConfig";
 import { Decano } from "../interfaces/decano.interface";
+import { fetchEstudiantes } from "../api/estudiante.api";
+import { useGrupos } from "./useGrupos";
 
 
 
@@ -57,65 +59,77 @@ interface ObjetosDeValidacion {
   setPendientes: (pendientes: ConfiguracionPendiente) => void
 }
 
-const mostrarNotificacion = ({ decano,semestre,estudiantes,pendientes,setPendientes }: ObjetosDeValidacion) => {
+const mostrarNotificacion = ({ decano,semestre,estudiantes,pendientes,grupos }: ObjetosDeValidacion) => {
   
+
+  let configPendientes = {...pendientes}
     if (!semestre || faltanFechas(semestre)) {
-      setPendientes({...pendientes,fechas: true})
+      configPendientes = {...pendientes,fechas: true}
       dispararNotificacion("Hay fechas pendientes de configurar",
         '<a href="/coordinador/calendario" style="color: blue;">Ir al calendario</a>')
       return;
   }
-    else {
-      setPendientes({...pendientes,fechas: false})
-  }
   
-  //TODO: Validar si hay asignaciones de grupo docente pendientes
-  setPendientes({ ...pendientes, asignacionesGrupoDocente: true })
+  if (grupos.find(g => !g.fechaEliminacion && !g.tutor)) {
+    configPendientes = { ...pendientes, asignacionesGrupoDocente:  true}
+    dispararNotificacion("Hay grupos sin asignar docente",
+      '<a href="/coordinador/configuraciones" style="color: blue;">Ir a configuraciones</a>')
+    return;
+   }
  
   if(estudiantes.length === 0){
-    setPendientes({ ...pendientes, estudiantes: true })
+    configPendientes = { ...pendientes, estudiantes: true }
     dispararNotificacion("No hay estudiantes cargados",
       '<a href="/coordinador/estudiantes" style="color: blue;">Ir a estudiantes</a>')
     return;
   }
-  else setPendientes({ ...pendientes, estudiantes: false })
 
   if (!decano || decano.nombre === '' || decano.numeroDocumento === '') {
-    setPendientes({ ...pendientes, decano: true })
+    configPendientes  ={ ...pendientes, decano: true }
     dispararNotificacion("No hay decano cargado",
       '<a href="/coordinador/configuraciones" style="color: blue;">Ir a configuraciones</a>')
     return;
   }
-  else setPendientes({ ...pendientes, decano: false })
 
+  console.log(configPendientes)
+  return configPendientes
 }
 export const useConfigNotificaciones = () => {
 
   const {user} = useAuth()
   const { semestre } = useSemestre()
-  const { estudiantes } = useEstudiantes()
+ const {grupos} = useGrupos()
+  console.log(grupos)
   const {decano} = useGeneralConfig()
   const [seMostro, setSeMostro] = useState<boolean>(false);
   //const [notificion, setNotificacion] = useState<Notificacion>('')
   const [pendientes, setPendientes] = useState<ConfiguracionPendiente>({
-    decano: true,
-    fechas: true,
-    asignacionesGrupoDocente: true,
-    estudiantes: true
+    decano: false,
+    fechas: false,
+    asignacionesGrupoDocente: false,
+    estudiantes: false
   })
-
+  const mostrar = async () => { 
+    
+    const estudiantes = await fetchEstudiantes()
+    const pendientesConf = mostrarNotificacion({
+      decano,
+      semestre,
+      pendientes,
+      estudiantes,
+      grupos
+    })
+    console.log(pendientesConf)
+    setPendientes(pendientesConf)
+   
+  }
   useEffect(() => { 
-    if (user?.roles.find(rol => rol.nombre === 'administrador') && !seMostro) {
+    if (user?.roles.find(rol => rol.nombre === 'administrador') && !seMostro && semestre && decano) {
+      console.log('mostrar')
       setSeMostro(true)
-      mostrarNotificacion({
-        decano,
-        semestre,
-        pendientes,
-        estudiantes,
-        setPendientes
-      })
+      mostrar()
     }
-  }, [semestre,user,decano,estudiantes,pendientes])
+  }, [semestre,user,decano])
 
   return {
     pendientes,
